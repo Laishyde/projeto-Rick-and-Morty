27c56bec60258de9fdc2be39441ee27cada43037
Laishyde <laiscristina.animamundi@gmail.com>
2022-11-27 22:57:16 -0300

    atualização de imagem

--- index.js
+++ index.js
@@ -2,22 +2,50 @@ class calcController {
     constructor() {
 
 
-        this.locale = 'pt-BR';
+        this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
-        this.initButtonsEvents();
+       
 
 
     }
     initialize() {
+       
+        
 
         setInterval(() => {
-            this.displayDate = this.currentDate.toLocaleDateString(this.locale);
-            this.displayTime = this.currentDate.toLocaleTimeString(this.locale);
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
 
         }, 1000);
     }
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value) {
+        return this._timeEl.innerHTML = value;
+    }
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+    set displayDate(value) {
+        return this._dateEl.innerHTML = value;
+    }
+    get _displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+    set _displayCalc(value) {
+        return this._displayCalcEl.innerHTML = value;
+    }
+    get currentDate(){
+        return new Date();
+
+    }
+    set currentDate(value){
+        return this._currentDate = value;
+    }
 }
+
